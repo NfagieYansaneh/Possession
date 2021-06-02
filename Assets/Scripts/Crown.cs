e0e1390158a87f40cf671a7a8f3ab3fcc03b6e89
NfagieYansaneh <60585677+NfagieYansaneh@users.noreply.github.com>
2021-06-03 00:38:23 +1200

    Proportional navigation properly implemented with minor *bugs*
    I finally completed some further tweaking on the proportional navigation for the crown prototype and now the crown is able to be thrown while locking on to nearby characters. However, the lock on is not too intense as to stop you from being able to 'select' who you wish to possess by throwing the crown at different initial trajectories. I do have some magic numbers in my code so I will come back later to fix on that. Interestingly, I found that the best way to make the crown deflections off walls not seem unpredicatable and frustating is to actually have the calculated deflection velocity to slightly rotate towards a character that was being tracked down. Thus, deflections deflected into a more desirable way, and that doesn't present frustation or the sense of RNG for the player.
    
    Although, on the other hand, there are some minor bugs. However, it is better to state this as niche cases where the crown doesnt act in a desirable way that could potentially seem unfair or buggy to the user. Like in some very small cases where the crown appears to deflect away from the player, or ignores sections of the ground, or can appear to track the wrong player. It should be stated that all of these bugs will be fixed, but not now, since it is in my best interest to further develop the Ai to see how my proportional navigation works with moving targets. However, this does not mean I wont fix a few bugs here and there for now (like collision detection).
    
    Either way, I am very happy about the results and want to see how animations, particle effects, and game feel will be once I implement complimenting components into my game.

--- Assets/Scripts/Crown.cs
+++ Assets/Scripts/Crown.cs
@@ -2,32 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEditor;
 
 public class Crown : MonoBehaviour
 {
-    public float speed;
-    public Rigidbody2D rb;
-    public LayerMask layerMaskCollision;
+    [Header("Basic variables")]
+    public float throwSpeed;
+    [HideInInspector]
+    public Vector2 velocity;
     // public AnimationCurve velocityChangeCurve; // how our speed changes overtime when our velocity changes (typically by deflections)
 
     // t_ variables are for timing purposes (and are hidden)
     // [HideInInspector] public float t_velocityChangeCurveTimestamp = 0f;
 
     public PlayerInputHandler playerInputHandler; // player input handler that controls this crown
+
+    [Header("Collision detection")]
+    [HideInInspector] public Rigidbody2D rb;
+    public LayerMask layerGroundMaskCollision;
+    public LayerMask layerCharacterMaskCollision;
     [HideInInspector]
     public Collider2D possessedPlayersCollider;
 
     [HideInInspector]
     public Vector2 direction;
 
-    public float characterAffinity = 1f; // TODO - add an editor button that allows individual character to override default values
+    [Header("Character Seeking & PN")]
+    public bool enablePN = true;
     public float characterAffnRange = 1f;
     public float characterAffnExitRange = 2f;
-    public List<BaseCharacterController> seekedCharacters = new List<BaseCharacterController>();
-    public LayerMask characterMask;
+    public float forceLockOnRadius = 0f; // if distance of the crown towards a seekedCharacter is lower than this, than the crown will be forced to seek that character
+    // as long as its within the crown 'cone' vision
 
+    public List<BaseCharacterController> seekedCharacters = new List<BaseCharacterController>();
+    public bool seeking = false;
+    BaseCharacterController seekedCharacter;
+
+    [Range(0f, 180f)]
+    public float cone;
+    public float maxAngCorr;
     public float dampening = 0.85f;
-    public Vector2 velocity;
+    Vector3 prevLOS;
+    Vector3 LOS;
+    Vector3 LOSGain;
+    float LOSAng;
+    //public LayerMask characterMask;
 
     public void Start()
     {
@@ -36,18 +55,51 @@ public class Crown : MonoBehaviour
         //gameObject.SetActive(false);
     }
 
-    public void Update()
-    {
-        //ApplyPorportionalNavigation();
-    }
+    Vector3 upperBound;
+    Vector3 lowerBound;
 
     public void FixedUpdate()
     {
-        // rb.velocity = velocity; // * velocityChangeCurve.Evaluate(Time.time - t_velocityChangeCurveTimestamp);
+        upperBound = transform.right * (characterAffnRange / 2);
+        lowerBound = transform.right * (characterAffnRange / 2);
+        upperBound = Vector3.RotateTowards(rb.velocity.normalized, Vector2.Perpendicular(rb.velocity.normalized), -cone * Mathf.Deg2Rad, 0.0f);
+        lowerBound = Vector3.RotateTowards(rb.velocity.normalized, -Vector2.Perpendicular(rb.velocity.normalized), -cone * Mathf.Deg2Rad, 0.0f);
+        dotProduct = Vector3.Dot(rb.velocity.normalized, upperBound);
+
         SeekCharacters();
-        ApplyPorportionalNavigation();
-        //ApplyPorportionalNavigation();
-        //GravitateToSeeked();
+        if (enablePN) ApplyPorportionalNavigation();
+    }
+
+    float dotProduct;
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + upperBound);
+        Gizmos.DrawLine(transform.position, transform.position + lowerBound);
+        //Handles.Label(transform.position + Vector3.up * 2f, Convert.ToString(dotProduct));
+
+        for (int i = 0; i < seekedCharacters.Count; i++)
+        {
+            Vector3 distanceHalf = (seekedCharacters[i].transform.position - transform.position) / 2;
+
+            Vector2 direction = seekedCharacters[i].transform.position - transform.position;
+            float dot = Vector2.Dot(direction.normalized, rb.velocity.normalized);
+
+            //Handles.Label(transform.position + distanceHalf, Convert.ToString(dot));
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(seekedCharacters[i].transform.position, forceLockOnRadius);
+
+            if (seekedCharacters[i] == seekedCharacter && seeking)
+            {
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawLine(transform.position, seekedCharacters[i].transform.position);
+                continue;
+            }
+
+            Gizmos.color = Color.white;
+            Gizmos.DrawLine(transform.position, seekedCharacters[i].transform.position);
+        }
     }
 
     public void ThrowMe(Vector2 desiredDirection, Collider2D newCollider)
@@ -63,8 +115,7 @@ public class Crown : MonoBehaviour
         else direction = desiredDirection;
         possessedPlayersCollider = newCollider;
 
-        rb.velocity = velocity = direction * speed;
-        // t_velocityChangeCurveTimestamp = Time.time;
+        rb.velocity = velocity = direction * throwSpeed;
     }
 
     public void SeekCharacters() // adds characters to the seeking list if they are within the character affinity range
@@ -84,8 +135,8 @@ public class Crown : MonoBehaviour
                 if (!seekedCharacters.Contains(baseCharacterController))
                 {
                     RaycastHit2D hit;
-                    hit = Physics2D.Raycast(transform.position, direction.normalized, characterAffnRange + 0.1f, layerMaskCollision); // detcting ground 
-                    if (hit == false)
+                    hit = Physics2D.Raycast(transform.position, direction.normalized, Mathf.Clamp(direction.magnitude, 0f, characterAffnRange), layerGroundMaskCollision); // detcting player
+                    if (hit == false && direction.magnitude < characterAffnRange)
                     {
                         seekedCharacters.Add(baseCharacterController);
                     }
@@ -102,80 +153,66 @@ public class Crown : MonoBehaviour
         }
     }
 
-    bool seeking = false;
-    BaseCharacterController closestCharacter;
-    Vector3 prevLOS;
-    Vector3 LOS;
-    Vector3 LOSGain;
-    float LOSAng;
-    public float maxAngCorr;
-
-    private void OnDrawGizmosSelected()
-    {
-        for(int i=0; i<seekedCharacters.Count; i++)
-        {
-            Vector3 distanceHalf = (seekedCharacters[i].transform.position - transform.position) / 2;
-
-            Vector2 direction = seekedCharacters[i].transform.position - transform.position;
-            float dot = Vector2.Dot(direction.normalized, rb.velocity.normalized);
-
-            UnityEditor.Handles.Label(transform.position + distanceHalf, Convert.ToString(dot));
-
-            if (seekedCharacters[i] == closestCharacter)
-            {
-                Gizmos.color = Color.cyan;
-                Gizmos.DrawLine(transform.position, seekedCharacters[i].transform.position);
-                continue;
-            }
-
-            Gizmos.color = Color.white;
-            Gizmos.DrawLine(transform.position, seekedCharacters[i].transform.position);
-        }
-    }
-
-    public void ApplyPorportionalNavigation()
+    public int ApplyPorportionalNavigation() // return 0 if it was able to seek a character, else returns -1
     {
         // Tutorial that taught me how to intergrate proportional navigation into my crown
         // https://www.moddb.com/members/blahdy/blogs/gamedev-introduction-to-proportional-navigation-part-i
         // https://answers.unity.com/questions/585035/lookat-2d-equivalent-.html
 
+        // previousDistance is the distance from the crown to the seekedCharacter
         float previousDistance = 0f;
+
+        // previousDot is the dot product from our crown's velocity and the direction towards the seekedCharacter
         float previousDot = 0f;
+
+        seeking = false;
         foreach (BaseCharacterController baseCharacterController in seekedCharacters)
         {
             Vector2 direction = baseCharacterController.transform.position - transform.position;
             float dot = Vector2.Dot(direction.normalized, rb.velocity.normalized);
-            //Debug.DrawRay(transform.position, rb.velocity.normalized, Color.yellow);
-            //Debug.DrawRay(transform.position, direction.normalized, Color.red);
 
             float distance = direction.magnitude;
-            if ((previousDot < dot && previousDistance > 1.3f) || 0.4f > Mathf.Abs(dot - previousDot) && distance < previousDistance || previousDistance == 0f)
+
+            // Vector2.dot takes in two vectors and tells us whether both vectors face the same direction, or opposite directions
+            // Vector2.dot returns a float that ranges from 1 to -1
+            // 1 means both vectors face in the exact same direction
+            // -1 means both vectors face in the exact opposite directions
+
+            // first case handles a case where there is only 1 potential seekedCharacter and just makes sure its in the crowns cone of vision
+            // second case handles a situation where our 'dot' is greater than the 'previousDot' but is not too far from the originally seekedCharacter
+            // third case handles a situation where our 'dot' is or is not greater than the 'previousDot' but is far closer to the originally seekedCharacter
+
+            if (dot >= dotProduct && !seeking ||
+                dot >= dotProduct && dot > previousDot && !(Vector2.Distance(transform.position, seekedCharacter.transform.position) <= forceLockOnRadius) ||
+                distance < forceLockOnRadius && !seeking ||
+                distance < forceLockOnRadius && seeking && distance < previousDistance)
             {
                 previousDot = dot;
                 seeking = true;
-                closestCharacter = baseCharacterController;
+                seekedCharacter = baseCharacterController;
                 previousDistance = distance;
+            }
+            else if (seekedCharacter == baseCharacterController) // can no longer seek that character because it does not pass any of the 3 cases above
+            {
+                Debug.LogWarning("hmmm");
+                seeking = false;
             }
         }
 
-        if (previousDistance == 0f)
-        {
-            seeking = false;
-            return;
-        }
+        if (!seeking) return -1;
 
         // Obtaining Line Of Sight (LOS) rotation rate
-        transform.right = closestCharacter.transform.position - transform.position;
+        transform.right = seekedCharacter.transform.position - transform.position;
 
-        LOS = closestCharacter.transform.position - transform.position;
+        LOS = seekedCharacter.transform.position - transform.position;
         LOSGain = LOS.normalized - prevLOS.normalized;
         LOSAng = Vector3.Angle(transform.right, LOS);
 
         Vector2 perpVec = Vector3.Cross(LOS.normalized, LOSGain.normalized); //perpVec is perpindicular to both the LOS and the LOS change
-        int N = 2;
+        int N = 3;
 
         float rotationSpeed = Mathf.Clamp(Mathf.Abs(LOSGain.magnitude * N), 0, maxAngCorr); //rotation_Speed = Omega * N (Clamped to maximum rotation speed)
-        if (LOSAng < 60)
+        if (LOSAng < cone) // used to be 60
         {
             Quaternion rot = Quaternion.AngleAxis(rotationSpeed, perpVec.normalized); // rotate with rotationSpeed around perpVec axis.
             //rb.velocity = Vector3.RotateTowards(rb.velocity, )
@@ -195,6 +232,8 @@ public class Crown : MonoBehaviour
         rb.velocity = Vector3.RotateTowards(rb.velocity, transform.right, maxAngCorr * Mathf.Deg2Rad, 0.0f);
         //Debug.Log(transform.right);
         //rb.rotation = transform.localRotation.z;
+
+        return 0;
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
@@ -215,7 +254,7 @@ public class Crown : MonoBehaviour
         else if (collider.gameObject.layer == 3) // else if it hits the ground, deflect off it
         {
             RaycastHit2D hit;
-            hit = Physics2D.Raycast(transform.position, rb.velocity.normalized, 1f, layerMaskCollision); // detcting ground 
+            hit = Physics2D.Raycast(transform.position, rb.velocity.normalized, 1f, layerGroundMaskCollision); // detcting ground 
             direction = Vector2.Reflect(rb.velocity.normalized, hit.normal);
 
 
@@ -225,12 +264,29 @@ public class Crown : MonoBehaviour
             //Debug.DrawRay(hit.point, direction, Color.yellow, 1f);
 
             rb.velocity = direction * rb.velocity.magnitude * dampening;
-            if (seeking)
+            if (seeking && enablePN)
             {
-                Vector3 x = closestCharacter.transform.position - transform.position;
-                if(x.magnitude < 1.5f) rb.velocity = Vector3.RotateTowards(rb.velocity, x.normalized, 78f * Mathf.Deg2Rad, 0.0f);
+                /*bool ready = false;
+                float previousDot = 0f;
+                float previousDistance = 0f;
+                foreach (BaseCharacterController potentialSeek in seekedCharacters)
+                {
+                    Vector2 direction = potentialSeek.transform.position - transform.position;
+                    float dot = Vector2.Dot(direction.normalized, rb.velocity.normalized);
+
+                    if (dot > previousDot || direction.magnitude < forceLockOnRadius && !ready ||
+                        direction.magnitude < forceLockOnRadius && direction.magnitude < previousDistance && ready)
+                    {
+                        ready = true;
+                        seekedCharacter = potentialSeek; 
+                    }
+                }*/
+
+                Vector3 x = seekedCharacter.transform.position - transform.position;
+                if(x.magnitude < forceLockOnRadius) rb.velocity = Vector3.RotateTowards(rb.velocity, x.normalized, 80f * Mathf.Deg2Rad, 0.0f);
+                else if (x.magnitude - 2f < forceLockOnRadius) rb.velocity = Vector3.RotateTowards(rb.velocity, x.normalized, 60f * Mathf.Deg2Rad, 0.0f);
                 else
-                rb.velocity = Vector3.RotateTowards(rb.velocity, x.normalized, 22f * Mathf.Deg2Rad, 0.0f);
+                rb.velocity = Vector3.RotateTowards(rb.velocity, x.normalized, 40f * Mathf.Deg2Rad, 0.0f);
             }
 
             velocity = rb.velocity;
